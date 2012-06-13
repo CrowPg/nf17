187a74daaef07f4b3b191a810f91a0092ab2e5c3
Johan <crow13pg@aol.fr>
2012-06-13 12:57:19 +0200

    Commit n°2

--- Tables_Requetes_SQL/Tables/Comptabilite.sql
+++ Tables_Requetes_SQL/Tables/Comptabilite.sql
@@ -2,6 +2,7 @@ CREATE TABLE Comptabilite(
 	dateDebut DATE,
 	dateFin DATE,
 	soldeDebut NUMERIC,
+	soldeCourant NUMERIC,
 	idDirigeant CHAR(10) REFERENCES Dirigeant(idDirigeant),
 	idComptabilite INTEGER,
 	PRIMARY KEY(idComptabilite)
--- Triggers.sql
+++ Triggers.sql
@@ -1,6 +1,56 @@
-CREATE OR REPLACE TRIGGER test_dir 
-AFTER UPDATE ON Comptabilite
+CREATE OR REPLACE TRIGGER Maj_Compta_Recette
+AFTER INSERT ON Recette
 FOR EACH ROW
 DECLARE
-	erreur EXCEPTION;
+	fin_exercice EXCEPTION;
+	date_fin_exercice DATE;
 	
+BEGIN
+	UPDATE Comptabilite c
+		SET c.soldeCourant = c.soldeCourant + :new.montant
+		WHERE c.idComptabilite = :new.idComptabilite;
+	SELECT dateFin INTO date_fin_exercice
+		FROM Comptabilite
+		WHERE Comptabilite.idComptabilite=:new.idComptabilite;
+	IF :new.DateRecette > date_fin_exercice then
+		RAISE fin_exercice;
+	END IF;
+END;
+
+CREATE OR REPLACE TRIGGER Maj_Compta_Service
+AFTER INSERT ON Service
+FOR EACH ROW
+DECLARE
+	fin_exercice EXCEPTION;
+	date_fin_exercice DATE;
+	
+BEGIN
+	UPDATE Comptabilite c
+		SET c.soldeCourant = c.soldeCourant - :new.montant
+		WHERE c.idComptabilite = :new.idComptabilite;
+	SELECT dateFin INTO date_fin_exercice
+		FROM Comptabilite
+		WHERE Comptabilite.idComptabilite=:new.idComptabilite;
+	IF :new.DateRecette > date_fin_exercice then
+		RAISE fin_exercice;
+	END IF;
+END;
+
+CREATE OR REPLACE TRIGGER Maj_Compta_Materiel
+AFTER INSERT ON Materiel
+FOR EACH ROW
+DECLARE
+	fin_exercice EXCEPTION;
+	date_fin_exercice DATE;
+	
+BEGIN
+	UPDATE Comptabilite c
+		SET c.soldeCourant = c.soldeCourant - :new.montant
+		WHERE c.idComptabilite = :new.idComptabilite;
+	SELECT dateFin INTO date_fin_exercice
+		FROM Comptabilite
+		WHERE Comptabilite.idComptabilite=:new.idComptabilite;
+	IF :new.DateRecette > date_fin_exercice then
+		RAISE fin_exercice;
+	END IF;
+END;
